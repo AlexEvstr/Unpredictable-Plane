20207ef23be3d159db088c8823997fcb5793f91c
Alexander Evstratov <alexdrugev@me.com>
2024-04-01 15:30:55 +0300

    Fix bugs.

--- Assets/_Scripts/PlaneCollision.cs
+++ Assets/_Scripts/PlaneCollision.cs
@@ -10,7 +10,10 @@ public class PlaneCollision : MonoBehaviour
         if (other.gameObject.CompareTag("Blade") || other.gameObject.CompareTag("Bomb"))
         {
             GameObject gameoverParticle = Instantiate(_gameoverParticle);
+            gameoverParticle.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
             Destroy(gameoverParticle, 3);
+
+            Destroy(other.gameObject);
             DisactivatePlane();
         }
     }
@@ -18,6 +21,7 @@ public class PlaneCollision : MonoBehaviour
     private void DisactivatePlane()
     {
         gameObject.GetComponent<MeshRenderer>().enabled = false;
+        gameObject.GetComponent<MeshCollider>().enabled = false;
         gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = false;
         gameObject.transform.GetChild(1).GetComponent<MeshRenderer>().enabled = false;
     }
