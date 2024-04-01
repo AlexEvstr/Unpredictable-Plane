0e08472345f0ab93423d15da7826b01d43208441
Alexander Evstratov <alexdrugev@me.com>
2024-04-01 22:14:40 +0300

    Add music and music toggle.

--- Assets/_Scripts/MenuManager.cs
+++ Assets/_Scripts/MenuManager.cs
@@ -6,6 +6,24 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private GameObject _skinsPanel;
     [SerializeField] private GameObject _levelsPanel;
 
+    [SerializeField] private GameObject _onIcon;
+    [SerializeField] private GameObject _offIcon;
+
+    private void Start()
+    {
+        AudioListener.volume = PlayerPrefs.GetFloat("music", 1);
+        if (AudioListener.volume == 0)
+        {
+            _onIcon.SetActive(false);
+            _offIcon.SetActive(true);
+        }
+        else
+        {
+            _onIcon.SetActive(true);
+            _offIcon.SetActive(false);
+        }
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("GameScene");
@@ -29,5 +47,21 @@ public class MenuManager : MonoBehaviour
     public void CloseLevelsPanel()
     {
         _levelsPanel.SetActive(false);
+    }
+
+    public void OffSound()
+    {
+        _onIcon.SetActive(false);
+        _offIcon.SetActive(true);
+        AudioListener.volume = 0;
+        PlayerPrefs.SetFloat("music", 0);
+    }
+
+    public void OnSound()
+    {
+        _onIcon.SetActive(true);
+        _offIcon.SetActive(false);
+        AudioListener.volume = 1;
+        PlayerPrefs.SetFloat("music", 1);
     }
 }
